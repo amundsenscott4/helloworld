95558c45b7dbd5d496eb530e2433b01057567b06
Kris <krzys.dabrowski@gmail.com>
2021-09-03 18:38:24 +0200

    multilicznik

--- multilicznik.cpp
+++ multilicznik.cpp
@@ -1,59 +1,70 @@
 #include <iostream>
 using namespace std;
 
-#define max 8
+#define max 128
 
 class multilicznik {
-    private:
-        int keys[8];
-        int values[8];
-        int size;
-    public:
-        void initialize() {
-            size = 0;
-        }
-        void print() {
-            for(int i=0; i<size; i++) {
-                cout << keys[i] << ", " << values[i] << "\n";
-            };
-        }
-        int increment(int k) {
-            for (int i=0; i<size; i++) {
-                if (keys[i] == k) {
-                    values[i]++;
-                    return values[i];
-                }    
-            };
+public:
+    int keys[max];
+    int values[max];
+    int size;
+    void initialize() {
+        size = 0;
+    }
+    void print() {
+        for(int i=0; i<size; i++) {
+            cout << keys[i] << ", " << values[i] << "\n";
+        };
+    }
+    int increment(int k) {
+        for (int i=0; i<size; i++) {
+            if (keys[i] == k) {
+                values[i]++;
+                return values[i];
+            }    
+        };
+        if (size < max) {
             keys[size] = k;
             values[size] = 1;
             size++;
             return 1;
         }
-        int check(int k) {
-            for (int i=0; i<size; i++) {
-                if (k==keys[i]) {
-                    return values[i];
-                }
-            }
+        else {
             return 0;
         }
+    }
+    int check(int k) {
+        for (int i=0; i<size; i++) {
+            if (k==keys[i]) {
+                return values[i];
+            }
+        }
+        return 0;
+    }
+    int operator+(int k) {
+        return increment(k);
+    }
 };
 
 int main() {
 
-    multilicznik c;
-    multilicznik d;
+    multilicznik c, d;
 
+    cout << "C:\n";
     c.initialize();
     c.increment(97);
+    c.operator+(97);
+    c + 96;
+    c.print();
 
+    cout << "D:\n";
     d.initialize();
     d.increment(97);
     d.increment(97);
-
-    c.print();
+    d.increment(98);
+    d.increment(99);
     d.print();
 
     return 0;
-    
+
 }
