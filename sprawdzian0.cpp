962dcc5346ab5e302e5ae35a8ad6b7d1f86c4d8f
Robert Dabrowski <r.dabrowski@uw.edu.pl>
2021-12-13 10:29:11 +0100

    podziel lepiej

--- sprawdzian0.cpp
+++ sprawdzian0.cpp
@@ -7,17 +7,21 @@ using namespace std;
 int const N=10;
 
 void Wypisz(int A[]) {
+    int suma = 0;
     for(int i=0; i<N; i++){
+        suma = suma + A[i];
         cout << A[i] << ", ";
     }
-    cout << endl;
+    cout << " (" << suma << ")" << endl;
 }
 
 void Wypisz(int A[], int start, int finish) {
+    int suma = 0;
     for(int i=start; i<=finish; i++){
+        suma = suma + A[i];
         cout << A[i] << ", ";
     }
-    cout << endl;
+    cout << " (" << suma << ")" << endl;
 }
 
 void Losuj(int A[]){
@@ -27,49 +31,46 @@ void Losuj(int A[]){
     }
 }
 
-// Zakladam, ze liczba istnieje w tej tablicy
-// i wtedy kazda z czesci jest niepusta
-int Podziel(int A[], int start, int finish, int liczba){
+int Podziel(int A[], int start, int finish){
+    if (start >= finish) { return finish; }
+    int v = finish;
     int i = start;
-    int j = finish;
-    int kubek = 0;
+    int j = finish-1;
+    int k = 0;
     while(i <= j) {
-        if( (A[i] < liczba) && (A[j] >= liczba) ){
+        if( (A[i] < A[v]) && (A[j] >= A[v]) ){
             i++;
             j--;
         }
-        if( (A[i] >= liczba) && (A[j] < liczba) ){
-            kubek = A[i];
-            A[i] = A[j];
-            A[j] = kubek;
+        if( (A[i] >= A[v]) && (A[j] < A[v]) ){
+            k = A[i]; A[i] = A[j]; A[j] = k;
             i++;
             j--;
         }
-        if( (A[i] < liczba) && (A[j] < liczba) ){
+        if( (A[i] < A[v]) && (A[j] < A[v]) ){
             i++;
         }
-        if( (A[i] >= liczba) && (A[j] >= liczba) ){
+        if( (A[i] >= A[v]) && (A[j] >= A[v]) ){
             j--;
         }
     }
+    k = A[i]; A[i] = A[v]; A[v] = k;
     return i;
 }
 
-void QuickSort(int A[], int s, int f) {
-    Wypisz(A, s, f);
-    if (s >= f) { return; }
-    // l musze wziac z tablic, np. A[s], A[f], A[(s+f)/2], itp.
-    int l = A[s];
-    int p = Podziel(A, s, f, l);
-    QuickSort(A, s, p-1);
-    QuickSort(A, p, f);
+void QuickSort(int A[], int start, int finish) {
+    if (start >= finish) { return; }
+    int p = Podziel(A, start, finish);
+    QuickSort(A, start, p-1);
+    QuickSort(A, p+1, finish);
 }
 
 int main(){
-    int A[N] = {8, 8, 6, 4, 1, 2, 8, 5, 1, 7};
-    // Losuj(A);
-    Wypisz(A, 0, 1);
-    QuickSort(A, 0, 9);
-    Wypisz(A, 0, 9);
+    int A[N]; //= {8, 1, 6, 4, 1, 2, 8, 5, 1, 5};
+    Losuj(A);
+    Wypisz(A);
+    QuickSort(A, 0, N-1);
+    // cout << Podziel(A, 0, 0) << endl;
+    Wypisz(A);
     return 0;
 }
