f14e77c5a3a3bb2d3d88d730d658589f9a007330
Robert Dabrowski <r.dabrowski@uw.edu.pl>
2022-01-16 16:47:01 +0100

    quicksort i listy

--- Python/quicksort.py
+++ Python/quicksort.py
@@ -1,52 +1,48 @@
 import random
 
-def PrintList(mylist):
-    for i in range(len(list)):
-        print(list[i])
-
-def Partition(mylist):
-    if len(mylist) == 1:
+def Partition(A):
+    if (len(A)==1):
         return 0
+    v = len(A)-1
     i = 0
-    j = len(mylist)-2
-    v = len(mylist)-1
+    j = len(A)-2
     while (i <= j):
-        if( (mylist[i] < mylist[v]) and (mylist[j] >= mylist[v]) ):
+        if ( (A[i] <  A[v]) and (A[j] >= A[v]) ):
             i += 1
             j -= 1
-        if( (mylist[i] >= mylist[v]) and (mylist[j] < mylist[v]) ):
-            mylist[i], mylist[j] = mylist[j], mylist[i]
+        if ( (A[i] >= A[v]) and (A[j] <  A[v]) ):
+            A[i], A[j] = A[j], A[i]
             i += 1
             j -= 1
-        if( (mylist[i] < mylist[v]) and (mylist[j] < mylist[v]) ):
+        if ( (A[i] <  A[v]) and (A[j] <  A[v]) ):
             i += 1
-        if( (mylist[i] >= mylist[v]) and (mylist[j] >= mylist[v]) ):
+        if ( (A[i] >= A[v]) and (A[j] >= A[v]) ):
             j -= 1
-    mylist[i], mylist[v] = mylist[v], mylist[i]
+    A[i], A[v] = A[v], A[i]
     return i
 
-def QuickSort(mylist):
-    print(mylist)
-    p = Partition(mylist)
-    left = mylist[:p-1]
-    mid = mylist[p:p]
-    right = mylist[p+1:]
+def QuickSort(A):
+    p = Partition(A)
+    # NIE: A[:p-1]
+    left = A[:p]
+    # A[p] pozostaje niezmieniony
+    right = A[p+1:]
     if len(left) > 0: 
         QuickSort(left)
     if len(right) > 0: 
         QuickSort(right)
-    mylist = left + mid + right
+    # NIE: A = left + mid + right
+    A[:p] = left
+    # A[p] pozostaje niezmieniony
+    A[p+1:] = right
 
 # list = [10, 9, 6, 3, 2]
 list = random.choices(range(1,100), k=10)
 
-print("Default: ")
+print("Input:")
 print(list)
-print("Partition on:")
-print(Partition(list))
-print("Partitioned:")
-print(list)
+print(sum(list))
 print("Quick sort:")
 QuickSort(list)
 print(list)
-
+print(sum(list))
