b2f7c445aaa45f451b5043a72c03116abf1a465b
Kris <krzys.dabrowski@gmail.com>
2022-05-07 11:09:39 +0200

    nawiasy1

--- 3M/nawiasystos.cpp
+++ 3M/nawiasystos.cpp
@@ -2,10 +2,35 @@
 #include <stack>
 using namespace std;
 
+bool Sprawdz(string a){
+    std::stack<char> stos;
+    int dlugosc = a.length();
+    for(int i=0; i<dlugosc; i++){
+        if(a[i] == '(') {
+            stos.push(a[i]);
+        }
+        if(a[i] == ')') {
+            if(stos.empty()){
+                return false;
+            }
+            else{
+                stos.pop();
+            }
+        }
+        else{
 
+        }
+    }
+    if(stos.empty()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
 int main() {
-    std::stack<int> stos;
-    char tab[10];
+    string nawiasy = ")(";
+    cout << Sprawdz(nawiasy);
     return 0;
 }
